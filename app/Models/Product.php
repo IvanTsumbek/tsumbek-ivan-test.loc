ea7994979d36ff0ed707ee4f39117510a6924b39
IvanTsumbek <maincumbek@gmail.com>
2026-02-25 21:11:39 +0200

    Relations, models, migrations

---
+++ app/Models/Product.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = ['name', 'description', 'price', 'category_id', 'image', 'slug'];
+
+    protected static function booted()
+    {
+        static::creating(function ($product) {
+            $product->slug = Str::slug($product->name);
+        });
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+}
